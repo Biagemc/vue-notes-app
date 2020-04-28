1e9523ec79a0e0d6b108012cce3e50f972d8e629
biagemc <biagemc@gmail.com>
2020-04-27 21:30:18 -0400

    allow active class to highlight based on click

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="note-selectors">
-    <note-selector class="active" />
-    <note-selector />
-    <note-selector />
+    <note-selector
+      v-for="note in notes"
+      v-bind:key="note.id"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+    />
   </div>
 </template>
 
@@ -13,6 +17,23 @@ export default {
   name: "note-selectors",
   components: {
     noteSelector,
+  },
+  data: function() {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() },
+        { id: 5, body: "This is a fifth test", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1,
+    };
+  },
+  methods: {
+    selectNote: function(note) {
+      this.selectedNoteId = note.id;
+    },
   },
 };
 </script>
