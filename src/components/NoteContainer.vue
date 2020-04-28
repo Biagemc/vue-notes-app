1bf6222bd4fcd88f83c95356c5deb3dc843267c2
biagemc <biagemc@gmail.com>
2020-04-27 20:39:26 -0400

    add container + editor + selectors + selector pages

---
+++ src/components/NoteContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="note-container">
+    <note-selectors />
+    <note-editor />
+  </div>
+</template>
+
+<script>
+import noteSelectors from "./NoteSelectors.vue";
+import noteEditor from "./NoteEditor.vue";
+
+export default {
+  name: "note-container",
+  components: {
+    noteSelectors,
+    noteEditor,
+  },
+};
+</script>
---
+++ src/components/NoteSelectors.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="note-selectors">
+    <note-selector class="active" />
+    <note-selector />
+    <note-selector />
+  </div>
+</template>
+
+<script>
+import noteSelector from "./NoteSelector.vue";
+
+export default {
+  name: "note-selectors",
+  components: {
+    noteSelector,
+  },
+};
+</script>
